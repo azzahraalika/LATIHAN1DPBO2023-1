7b391f8cf2bb9a6e3476db1d55305985d32a51d1
Raisyad Jullfikar <raisyadjullfikar@gmail.com>
2023-02-14 21:31:56 +0700

    Latihan 1

---
+++ PHP/index.php
@@ -0,0 +1,35 @@
+<!-- Saya Raisyad Jullfikar NIM 2106238 mengerjakan Latihan 1 dalam mata kuliah Desain dan Pemrograman Berorientasi  -->
+<!-- Objek untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin. -->
+<?php
+// Import File
+  require('Mahasiswa.php');
+  require('Crud.php');
+  $MchnCrud = new Crud(); // Init objek crud
+  $countData = 5;
+  for ($i = 0; $i < $countData; $i++) {
+    $Input[$i] = new Mahasiswa(); // Init objek mhs
+    
+    // Create Data
+    $MchnCrud->Create($Input[$i], 'Anak Ilkom ' . $i, rand(2100000,2109999), 'Ilkom', 'FPMIPA', './img/img_' . $i . '.jpg');
+  }
+  // Read Data
+  $MchnCrud->Read($Input, 1, $countData);
+  echo "</br>" . "</br>" . "</br>";
+  
+  // Init Objek arr 5 & 6
+  $Input[5] = new Mahasiswa();
+  $Input[6] = new Mahasiswa();
+  
+  // Create Data
+  $MchnCrud->Create($Input[5], 'Anak Ilkom 5', '2106238', 'Ilkom', 'FPMIPA', './img/img_5.jpg');
+  $countData++;
+  $MchnCrud->Create($Input[6], 'Anak Ilkom 6', '2106531', 'Ilkom', 'FPMIPA', './img/img_6.jpg');
+  $countData++;
+
+  // Update and delete data
+  $MchnCrud->Update($Input, '2106238', 'Anak Ilkom 7', '2107645', 'Bim. Konseling', 'FIP', './img/img_0.jpg');
+  $MchnCrud->Delete($Input, '2106531', $countData);
+
+  // Read Data
+  $MchnCrud->Read($Input, 2, $countData);
+?>
